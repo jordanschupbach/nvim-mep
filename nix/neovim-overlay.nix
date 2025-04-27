8437ef4776e8ddf99b38e517e323e8de23be8b7b
jordanschupbach <jordans1882@gmail.com>
2025-04-27 15:30:41 -0600

    Add nvim-tree dependency

--- nix/neovim-overlay.nix
+++ nix/neovim-overlay.nix
@@ -33,6 +33,7 @@ with final.pkgs.lib; let
     nvim-treesitter.withAllGrammars
     luasnip # snippets | https://github.com/l3mon4d3/luasnip/
     # nvim-cmp (autocompletion) and extensions
+    nvim-tree #
     nvim-cmp # https://github.com/hrsh7th/nvim-cmp
     cmp_luasnip # snippets autocompletion extension for nvim-cmp | https://github.com/saadparwaiz1/cmp_luasnip/
     lspkind-nvim # vscode-like LSP pictograms | https://github.com/onsails/lspkind.nvim/
