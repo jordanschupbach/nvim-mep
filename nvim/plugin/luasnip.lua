425a6ef35952190067b8f30f5ece3237d702193e
jordan <jordans1882@gmail.com>
2025-06-28 09:54:58 -0600

    Fix helloworld and add nix module binding

--- nvim/plugin/luasnip.lua
+++ nvim/plugin/luasnip.lua
@@ -590,20 +590,60 @@ ls.add_snippets("cpp", {
       "#include <iostream>",
       "",
       "int main(void) {",
-      "  std::cout<< \"Hello World!\"<<std::endl;",
+      "  std::cout << \"Hello World!\" << std::endl;",
       "  return 0;",
       "}"
     }),
   }),
 
 
+}, {
+  key = "cpp",
+})
+-- }}} Cpp snippets
 
+-- {{{ CPP snippets
+ls.add_snippets("nix", {
+
+
+  -- {{{ Module
+  s("module", {
+    t({
+      "{",
+      "  imports = [",
+      "    # Paths to other modules.",
+      "    # Compose this module out of smaller ones.",
+      "  ];",
+      "",
+      "  options = {",
+      "    # Option declarations.",
+      "    # Declare what settings a user of this module can set.",
+      "    # Usually this includes a global \"enable\" option which defaults to false.",
+      "  };",
+      "",
+      "  config = {",
+      "    # Option definitions.",
+      "    # Define what other settings, services and resources should be active.",
+      "    # Usually these depend on whether a user of this module chose to \"enable\" it",
+      "    # using the \"option\" above.",
+      "    # Options for modules imported in \"imports\" can be set here.",
+      "  };",
+      "}",
+    }),
+  -- }}} Module
+
+
+
+  }),
 
 
 }, {
-  key = "java",
+  key = "nix",
 })
--- }}} Java snippets
+-- }}} Cpp snippets
+
+
+
 
 
 
--- playground/cpp/main.cpp
+++ playground/cpp/main.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 
 int main(void) {
-  std::cout << "Hello, World!" << std::endl;
+  std::cout << "Hello World!" << std::endl;
   return 0;
 }
 
