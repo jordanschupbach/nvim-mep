fc4e97a752f8df861e9facb8953f00e205a8182b
jordan <jordans1882@gmail.com>
2025-07-12 16:45:01 -0600

    Add nix basic shell snippet

--- nvim/plugin/luasnip.lua
+++ nvim/plugin/luasnip.lua
@@ -649,6 +649,48 @@ ls.add_snippets("nix", {
     }),
   }),
 
+
+
+  s("basic-shell", {
+    t({
+      '{',
+      '  description = "A basic flake";',
+      '  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";',
+      '  inputs.systems.url = "github:nix-systems/default";',
+      '  inputs.flake-utils = {',
+      '    url = "github:numtide/flake-utils";',
+      '    inputs.systems.follows = "systems";',
+      '  };',
+      '',
+      '  outputs =',
+      '    { nixpkgs, flake-utils, ... }:',
+      '    flake-utils.lib.eachDefaultSystem (',
+      '      system:',
+      '      let',
+      '        pkgs = nixpkgs.legacyPackages.${system};',
+      '      in',
+      '      {',
+      '        devShells.default = pkgs.mkShell { ',
+      '          packages = [ pkgs.hello ]; ',
+      '        };',
+      '      }',
+      '    );',
+      '}',
+    }),
+  }),
+
+
+
+
+
+
+
+
+
+
+
+
+
 }, {
   key = "nix",
 })
