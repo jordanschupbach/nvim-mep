152703344a4887598ce88e390abed9e82193b6cb
jordanschupbach <jordans1882@gmail.com>
2025-07-13 17:35:53 -0600

    Add hook to set filetype to fortran

--- nvim/init.lua
+++ nvim/init.lua
@@ -1151,3 +1151,16 @@ mymap('n', '<A-C-k>', "<CMD>lua require('smart-splits').swap_buf_up()<CR>")
 mymap('t', '<A-C-k>', "<CMD>lua require('smart-splits').swap_buf_up()<CR>")
 
 
+
+-- {{{ Fortran filetype detection
+vim.api.nvim_create_autocmd({"BufNewFile", "BufRead"}, {
+    pattern = "*.f90",
+    callback = function()
+        vim.bo.filetype = "fortran"
+    end,
+})
+-- }}} Fortran filetype detection
+
+
+
+
