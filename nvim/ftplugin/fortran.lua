da491825054855fef36bc32efe44f24d195f2fe4
jordanschupbach <jordans1882@gmail.com>
2025-07-13 17:40:28 -0600

    Add fortran lsp config

--- nvim/ftplugin/fortran.lua
+++ nvim/ftplugin/fortran.lua
@@ -1,3 +1,19 @@
 
 vim.cmd("echo 'Entering fortran ftplugin'")
 
+
+
+
+local root_files = {
+  '.git',
+}
+
+vim.lsp.start {
+  name = 'fortran',
+  root_dir = vim.fs.dirname(vim.fs.find(root_files, { upward = true })[1]),
+  cmd = { 'fortls' },
+  root_markers = { '.git' },
+  filetypes = { 'f', 'f90' },
+  capabilities = require('user.lsp').make_client_capabilities(),
+}
+
