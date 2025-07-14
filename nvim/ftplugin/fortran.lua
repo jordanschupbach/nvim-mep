ef110b9a449079ec1ea25ccd0925ad4f1949513a
jordanschupbach <jordans1882@gmail.com>
2025-07-13 19:48:40 -0600

    Test using buildin

--- nvim/ftplugin/fortran.lua
+++ nvim/ftplugin/fortran.lua
@@ -8,12 +8,14 @@ local root_files = {
   '.git',
 }
 
-vim.lsp.start {
-  name = 'fortran',
-  root_dir = vim.fs.dirname(vim.fs.find(root_files, { upward = true })[1]),
-  cmd = { 'fortls' },
-  root_markers = { '.git' },
-  filetypes = { 'f', 'f90' },
-  capabilities = require('user.lsp').make_client_capabilities(),
-}
+vim.lsp.enable('fortls')
+
+-- vim.lsp.start {
+--   name = 'fortran',
+--   root_dir = vim.fs.dirname(vim.fs.find(root_files, { upward = true })[1]),
+--   cmd = { 'fortls' },
+--   root_markers = { '.git' },
+--   filetypes = { 'f', 'f90' },
+--   capabilities = require('user.lsp').make_client_capabilities(),
+-- }
 
