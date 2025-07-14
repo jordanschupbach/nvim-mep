9e1136c5c3442b9d2ee73f92cb0e404885f60b92
jordanschupbach <jordans1882@gmail.com>
2025-07-13 22:04:28 -0600

    Minor changes to bashls config

--- nvim/ftplugin/bash.lua
+++ nvim/ftplugin/bash.lua
@@ -3,8 +3,8 @@
 -- vim.lsp.enable('bashls')
 
 vim.lsp.start {
-  name = 'bashls',
-  root_dir = vim.fs.dirname(vim.fs.find(root_files, { upward = true })[1]),
+  name = 'shell',
+  -- root_dir = vim.fs.dirname(vim.fs.find(root_files, { upward = true })[1]),
   cmd = { 'bash-language-server', 'start' },
   root_markers = { '.git' },
   filetypes = { 'bash', 'sh'},
