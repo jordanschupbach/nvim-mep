7ee086bc577e6f04a777980ec857de37e7694334
jordanschupbach <jordans1882@gmail.com>
2025-07-13 19:58:49 -0600

    Add jedi language server

--- nvim/ftplugin/python.lua
+++ nvim/ftplugin/python.lua
@@ -6,5 +6,21 @@ local function mymap(mode, key, value)
   vim.keymap.set(mode, key, value, { silent = true, remap = true })
 end
 
+local root_files = {
+  '.git',
+}
+
+
+vim.lsp.start {
+  name = 'jedi',
+  root_dir = vim.fs.dirname(vim.fs.find(root_files, { upward = true })[1]),
+  cmd = { 'jedi-language-server' },
+  root_markers = { '.git' },
+  filetypes = { 'python' },
+  capabilities = require('user.lsp').make_client_capabilities(),
+}
+
+
+
 
 
