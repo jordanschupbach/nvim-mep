1370662e3da141c21e08984336a7dc14c8ed3652
jordanschupbach <jordans1882@gmail.com>
2025-08-01 21:19:40 -0600

    maybe fix cpp example runner command

--- nvim/lua/overseer/template/user/cpp_build.lua
+++ nvim/lua/overseer/template/user/cpp_build.lua
@@ -6,8 +6,7 @@ return {
     local filename = vim.fn.fnamemodify(file, ":t")
     local example_name = filename:gsub("^prefix_cpp_", ""):gsub("%.cpp$", "")
     return {
-      cmd = { "just" },
-      args = {"run", "example " .. example_name .. "_cpp" },
+      cmd = { "just ", "run ", "example " .. example_name .. "_cpp"  },
       components = { { "on_output_quickfix", open = true }, "default" },
     }
   end,
