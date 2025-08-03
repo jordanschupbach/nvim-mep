d28d13a18a2098fd740215346188d6d27d71dc65
jordanschupbach <jordans1882@gmail.com>
2025-08-03 09:11:11 -0600

    add none-ls and pylint config

--- nix/neovim-overlay.nix
+++ nix/neovim-overlay.nix
@@ -158,6 +158,7 @@ with final.pkgs.lib; let
     vim-slime
     which-key-nvim
     zen-mode-nvim
+    none-ls-nvim
 
     # # neorg
     # vim-unimpaired # predefined ] and [ navigation keymaps | https://github.com/tpope/vim-unimpaired/
