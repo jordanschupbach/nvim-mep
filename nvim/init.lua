9cd27ba5a7b8eda5f10526beb091783c53bbdb08
jordanschupbach <jordans1882@gmail.com>
2025-08-05 15:24:53 -0600

    minor mods to slime send

--- nvim/init.lua
+++ nvim/init.lua
@@ -1109,20 +1109,21 @@ wrapped_slime = function()
   local success, err = pcall(function()
     vim.cmd('sleep 10m') -- Adjust the sleep as necessary
     vim.cmd("'<,'>SlimeSend") -- Send to Slime
+    vim.cmd('sleep 10m') -- Adjust the sleep as necessary
   end)
 
   if not success then
-    print("Error sending to Slime: " .. err) -- Handle the error gracefully
+    print("Error sending to Slime: " .. err .. "(just try again if cause registers not set)") -- Handle the error gracefully
   end
 
   -- Second attempt
-  success, err = pcall(function()
-    vim.cmd('sleep 10m') -- Adjust the sleep as necessary
-    vim.cmd("'<,'>SlimeSend") -- Send to Slime
-  end)
+  -- success, err = pcall(function()
+  --   vim.cmd('sleep 10m') -- Adjust the sleep as necessary
+  --   vim.cmd("'<,'>SlimeSend") -- Send to Slime
+  -- end)
 
   if not success then
-    print("Error in second send to Slime: " .. err .. "(just try again if cause registers not set)") -- Handle the error gracefully
+    print("Error in second send to Slime: " .. err) -- Handle the error gracefully
   end
 end
 
