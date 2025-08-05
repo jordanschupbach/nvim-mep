c70ec5d4ec88246b70abdd7c6909b91ff4ef6399
jordanschupbach <jordans1882@gmail.com>
2025-08-05 07:56:16 -0600

    Make slim wrapped

--- nvim/plugin/slime.lua
+++ nvim/plugin/slime.lua
@@ -1,6 +1,12 @@
-
 local function mymap(mode, key, value)
   vim.keymap.set(mode, key, value, { silent = true, remap = true })
 end
 
-mymap('n', '<A-return>', '<CMD>SlimeSend<CR>')
+
+local wrapped_slime = function()
+  vim.cmd('sleep 10m')      -- Adjust the sleep as necessary
+  vim.cmd("'<,'>SlimeSend") -- Send to Slime
+end
+
+mymap('n', '<A-return>', ':lua wrapped_slime()<CR>')
+-- mymap('n', '<A-return>', '<CMD>SlimeSend<CR>')
