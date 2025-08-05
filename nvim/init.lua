cf5e28b4ebb3637e4e3e859bd68a59a6b78d6704
jordanschupbach <jordans1882@gmail.com>
2025-08-05 14:57:29 -0600

    modify visual slime send

--- nvim/init.lua
+++ nvim/init.lua
@@ -1772,7 +1772,7 @@ end
 mymap('n', '<Space>bi', '<CMD>lua show_buffer_info()<CR>')
 -- mymap('n', '<A-return>', '<CMD>lua send_line_to_buffer()<CR>')
 mymap('n', '<A-return>', '<CMD>SlimeSend<CR>')
-mymap('v', '<A-return>', '<CMD>SlimeSend<CR>')
+mymap('v', '<A-return>', "<CMD>'<,'>SlimeSend<CR>")
 -- mymap('v', '<A-return>', "<CMD>'<,'>SlimeSend<CR>")
 -- mymap('v', '<A-return>', '<CMD>lua send_lines_to_buffer()<CR>')
 
